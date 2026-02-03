651610b8ed07e0956273996a74613a97468c576d
yangheng <931219874@qq.com>
2026-02-03 19:29:38 +0800

    fix: Update language icon in header to use material design style

--- mail-vue/src/layout/header/index.vue
+++ mail-vue/src/layout/header/index.vue
@@ -17,7 +17,7 @@
         <Icon icon="solar:moon-linear"/>
       </div>
       <div class="lang-icon icon-item" @click="toggleLang">
-        <Icon icon="mingcute:translate_line"/>
+        <Icon icon="material-symbols:language"/>
       </div>
       <div class="notice icon-item" @click="openNotice">
         <Icon icon="streamline-plump:announcement-megaphone"/>
